04378a5fe23f2e7d35348d8d876754b35d81eab1
IsraelMobote <moboteisrael02@gmail.com>
2024-06-24 14:41:32 +0100

    edit

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,3 +1,15 @@
+const year = document.querySelector("#currentYear");
+
+let lastModified = document.querySelector("#lastModified")
+
+const today = new Date();
+// this statement below is to get the current year in the footer
+year.innerHTML = today.getFullYear();
+
+// this statement is to get the last modified string in the footer
+lastModified.innerHTML = new Date(document.lastModified);
+
+
 const navi = document.querySelector('#navi');
 const menu = document.querySelector('#menuBar');
 
