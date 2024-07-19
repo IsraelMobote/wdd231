cf9fbbb3861cef69bb8954f45644852a1761f949
IsraelMobote <moboteisrael02@gmail.com>
2024-07-19 05:46:57 +0100

    edit

--- scripts/modules.js
+++ scripts/modules.js
@@ -1,13 +1,26 @@
 
 import aCourse from "./courses.mjs";
 
-  document.querySelector("#enrollStudent").addEventListener("click", function () {
-    const sectionNum = document.querySelector("#sectionNumber").value;
-    aCourse.changeEnrollment(sectionNum);
-  });
-  document.querySelector("#dropStudent").addEventListener("click", function () {
-    const sectionNum = document.querySelector("#sectionNumber").value;
-    aCourse.changeEnrollment(sectionNum, false);
-  });
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.changeEnrollment(sectionNum);
+})
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.changeEnrollment(sectionNum, false);
+});
 
-  aCourse.init();
+aCourse.init();
+
+
+const url = 'https://api.thedogapi.com/v1/breeds';
+const url1 = '';
+
+
+try {
+  const response = await fetch(url);
+  const result = await response.json();
+  console.log(result);
+} catch (error) {
+  console.error(error);
+}
